699c04895a34da2667bc2b94829e80dc19e0d126
PietroDSouzaRangel <desouzarangelpietro344@gmail.com>
2026-09-23 12:51:29 -0300

    update src/pages/Home/types.ts

---
+++ src/pages/Home/types.ts
@@ -0,0 +1,38 @@
+export interface Entidade {
+    id: number;
+    x: number;
+    y: number;
+    tipo: 'inimigo' | 'vida' | 'meteoro' | 'escudo' | 'laser_duplo' | 'bomba';
+}
+
+export interface Tiro {
+    id: number;
+    x: number;
+    y: number;
+    tipo?: 'normal' | 'duplo';
+}
+
+export interface Particula {
+    id: number;
+    x: number;
+    y: number;
+    texto: string;
+    cor: string;
+}
+
+export interface Chefao {
+    ativo: boolean;
+    x: number;
+    y: number;
+    vida: number;
+    vidaMax: number;
+    direcao: number;
+}
+
+export interface Nave {
+    id: string;
+    nome: string;
+    corBorda: string;
+    corBg: string;
+    icone: any;
+}
